e694be4c76cc16703d1f0cee8c70fe9247ac5440
taeeon kim <myhom43@gmail.com>
2023-01-15 23:29:12 +0900

    feat: static 파일 추가, use express.static 추가

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
 // get을 router라 부르고 하는일을 routing 이라 표현한다.
 app.get("/", (req, res) => {
   res.send("Hello Home Page");
 });
+app.get("/route", (req, res) => {
+  res.send("hello router, <img src='/choonsik.jpeg'>");
+});
 app.get("/login", (req, res) => {
-  res.send("Login");
+  res.send("<h1>Login</h1>");
 });
 app.listen(3000, function () {
   console.log("Connected to 3000 port");
