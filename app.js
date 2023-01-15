22f3b5c3fa83f98bb2cdeaddd9e6c2bc1f49d699
taeeon kim <myhom43@gmail.com>
2023-01-16 00:09:15 +0900

    feat: add jade setting, writing jade syntax

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require("express");
 var app = express();
-
+app.locals.pretty = true;
+app.set("view engine", "jade"); // 약속된 STRING view engine, jade engine으로 템플릿 사용 지정
+app.set("views", "./views"); // 관습적으로 views 폴더를 사용 하지만 이름바꿔도됨
 app.use(express.static("public"));
+app.get("/template", (req, res) => {
+  res.render("temp", { time: Date(), _title: "templae" });
+});
 // get을 router라 부르고 하는일을 routing 이라 표현한다.
 app.get("/", (req, res) => {
   res.send("Hello Home Page");
