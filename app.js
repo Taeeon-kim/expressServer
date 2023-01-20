7c0b8662c1a54a290ff8347609332e3423821b70
taeeon kim <myhom43@gmail.com>
2023-01-21 04:26:40 +0900

    feat: text형식이 아닌 html 파일을 보내는걸로 변경, 각 페이지 html 추가

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const path = require('path');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
