8d26adf4f7c18ce5442dc4ff8bdbb00b59010c18
taeeon kim <myhom43@gmail.com>
2023-01-20 17:08:17 +0900

    feat: 서버 셋팅, POST 비동기처리

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
-const http = require("http");
-function rqListener(req, res) {}
+const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></h1></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split('=')[1];
+      fs.writeFile('message.txt', message, (err) => {
+        console.log(err);
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        console.log('here');
+        return res.end();
+      });
+    });
+  }
+  console.log('!1');
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My first request</title></head>');
+  res.write('<body><h1>Hello from my node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
   //   process.exit();
 });
 
