5b5b1de5c32f730e292cab6f781dce59ef225f48
taeeon kim <myhom43@gmail.com>
2023-01-21 04:01:59 +0900

    feat: admin 공용 라우트 path 추가

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(adminRoutes);
 app.use((req, res, next) => {
   res.status(404).send('<h1>Page not found</h1>');
 });
