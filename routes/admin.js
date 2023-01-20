fdf88113f7d7719af6178a26003e0856cd4e4619
taeeon kim <myhom43@gmail.com>
2023-01-21 04:51:52 +0900

    feat: root file 경로 설정 dirname

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const express = require('express');
-
+const rootDir = require('../util/path');
 const router = express.Router();
 
 //admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   // console.log('In another Middleware!');
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html')); // 현재경로에서 ../ 뒤로가서 views 폴더의 add-product.html을 보내줌
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html')); // 현재경로에서 ../ 뒤로가서 views 폴더의 add-product.html을 보내줌
 });
 
 //admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const express = require('express');
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   // console.log('In the Middleware!');
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')); // join 을 쓰는 이유는 윈도우와 리눅스 시스템에서 시스템 경로가 윈도우는 /, 리눅스는 \ 로 쓰기 때문에 이렇게 사용하는 것
+  res.sendFile(path.join(rootDir, 'views', 'shop.html')); // join 을 쓰는 이유는 윈도우와 리눅스 시스템에서 시스템 경로가 윈도우는 /, 리눅스는 \ 로 쓰기 때문에 이렇게 사용하는 것
   // next(); // 다음 use 미들웨어를 계속 이어가기위한 요청을 허용해준다
 }); // 미들웨어함수 추가,
 
