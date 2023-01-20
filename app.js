3c7ffe78061206cf64963433dc53ed304f9d89b5
taeeon kim <myhom43@gmail.com>
2023-01-21 03:52:11 +0900

    feat: 404 페이지 route 미들웨어 추가

--- app.js
+++ app.js
@@ -7,5 +7,8 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(shopRoutes);
 app.use(adminRoutes);
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(3000);
