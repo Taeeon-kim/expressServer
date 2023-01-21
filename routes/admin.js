39b86867e8b99e3827d8df455a3876eabaed34fa
taeeon kim <myhom43@gmail.com>
2023-01-21 18:39:21 +0900

    feat: 동적 페이지 css active 조건, title 추가

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,10 @@ const products = [];
 router.get('/add-product', (req, res, next) => {
   // console.log('In another Middleware!');
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html')); // 현재경로에서 ../ 뒤로가서 views 폴더의 add-product.html을 보내줌
-  res.render('add-product', { pageTitle: 'Add Product' });
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+  });
 });
 
 //admin/add-product => POST
