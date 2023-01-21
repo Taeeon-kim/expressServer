6074c71522df611d855f7908024a2a621aabbc7e
taeeon kim <myhom43@gmail.com>
2023-01-22 08:04:36 +0900

    fix: error path 추가

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error')
 const path = require('path');
 
 const app = express();
@@ -13,9 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))); // 미들웨어 정적파일 폴더 지정
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  res.status(404).render('404', { pageTitle: '404' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
