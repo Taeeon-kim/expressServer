9b39de89af6c01cb59a1877715b93a0ade4b6554
taeeon kim <myhom43@gmail.com>
2023-01-24 02:57:25 +0900

    feat: mysql 추가

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error')
+const errorController = require('./controllers/error');
 const path = require('path');
-
+const db = require('./util/database');
 const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+db.execute('SELECT * FROM products')
+  .then((result) => {
+    console.log(result[0]);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))); // 미들웨어 정적파일 폴더 지정
