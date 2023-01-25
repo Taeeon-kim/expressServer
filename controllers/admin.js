7c38cd99a204289f179f44d9666ead5ada01a073
taeeon kim <myhom43@gmail.com>
2023-01-26 04:55:09 +0900

    fix: sql => nosql, mongodb 로변경

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,69 +10,16 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
-exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit;
-  if (!editMode) {
-    return res.redirect('/');
-  }
-  const prodId = req.params.productId;
-  req.user.getProducts({where: {id:prodId}})
-  // Product.findByPk(prodId)
-    .then((products) => {
-      console.log(products)
-      const product = products[0]
-      if (!product) {
-        return res.redirect('/');
-      }
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-
-exports.postEditProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  const updatedTitle = req.body.title;
-  const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
-  const updatedDescription = req.body.description;
-  Product.findByPk(prodId)
-    .then((product) => {
-      product.title = updatedTitle;
-      product.description = updatedDescription;
-      product.imageUrl = updatedImageUrl;
-      product.price = updatedPrice;
-      console.log(product, 'product!!!!');
-      return product.save();
-    })
-    .then((result) => {
-      console.log('Updated Product');
-      res.redirect('/admin/products');
-    })
-    .catch((err) => console.log(err));
-};
-
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user
-    .createProduct({
-      title: title,
-      imageUrl: imageUrl,
-      price: price,
-      description: description,
-      userId: req.user.id,
-    })
+  const product = new Product({ title, price, description, imageUrl });
+  product
+    .save()
     .then((result) => {
-      // console.log(result);
+      console.log(result);
       console.log('Created Product');
       res.redirect('/admin/products');
     })
@@ -81,29 +28,78 @@ exports.postAddProduct = (req, res, next) => {
     });
 };
 
-exports.getProducts = (req, res, next) => {
-  // Product.findAll()
-  req.user.getProducts()
-    .then((products) => {
-      console.log(products);
-      res.render('admin/products', {
-        prods: products,
-        pageTitle: 'Admin Product',
-        path: '/admin/products',
-      }); //app.set() 템플릿 셋팅하였기 때문에 파일명만 쓰면됨
-    })
-    .catch((err) => console.log(err));
-};
+// exports.getEditProduct = (req, res, next) => {
+//   const editMode = req.query.edit;
+//   if (!editMode) {
+//     return res.redirect('/');
+//   }
+//   const prodId = req.params.productId;
+//   req.user.getProducts({where: {id:prodId}})
+//   // Product.findByPk(prodId)
+//     .then((products) => {
+//       console.log(products)
+//       const product = products[0]
+//       if (!product) {
+//         return res.redirect('/');
+//       }
+//       res.render('admin/edit-product', {
+//         pageTitle: 'Edit Product',
+//         path: '/admin/edit-product',
+//         editing: editMode,
+//         product: product,
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findByPk(prodId)
-    .then((product) => {
-      return product.destroy();
-    })
-    .then((result) => {
-      console.log('Product deleted', result);
-      res.redirect('/admin/products');
-    })
-    .catch((err) => console.log(err));
-};
+// exports.postEditProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   const updatedTitle = req.body.title;
+//   const updatedPrice = req.body.price;
+//   const updatedImageUrl = req.body.imageUrl;
+//   const updatedDescription = req.body.description;
+//   Product.findByPk(prodId)
+//     .then((product) => {
+//       product.title = updatedTitle;
+//       product.description = updatedDescription;
+//       product.imageUrl = updatedImageUrl;
+//       product.price = updatedPrice;
+//       console.log(product, 'product!!!!');
+//       return product.save();
+//     })
+//     .then((result) => {
+//       console.log('Updated Product');
+//       res.redirect('/admin/products');
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+// exports.getProducts = (req, res, next) => {
+//   // Product.findAll()
+//   req.user
+//     .getProducts()
+//     .then((products) => {
+//       console.log(products);
+//       res.render('admin/products', {
+//         prods: products,
+//         pageTitle: 'Admin Product',
+//         path: '/admin/products',
+//       }); //app.set() 템플릿 셋팅하였기 때문에 파일명만 쓰면됨
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+// exports.postDeleteProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   Product.findByPk(prodId)
+//     .then((product) => {
+//       return product.destroy();
+//     })
+//     .then((result) => {
+//       console.log('Product deleted', result);
+//       res.redirect('/admin/products');
+//     })
+//     .catch((err) => console.log(err));
+// };
