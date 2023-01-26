6f559c38510939ada3b3e577bb702b41bf36e70b
taeeon kim <myhom43@gmail.com>
2023-01-27 01:42:36 +0900

    feat: 유저 장바구니 담기 추가

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 const adminRoutes = require('./routes/admin');
@@ -15,15 +16,22 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))); // 미들웨어 정적파일 폴더 지정
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then((user) => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
-  next();
+  User.findById('63d295cc79b138a17f73ffa5')
+    .then((user) => {
+      console.log(user)
+      // console.log("app user: ",user)
+      req.user = new User({
+        username: user.name,
+        email: user.email,
+        cart: user.cart,
+        id: user._id,
+      });
+      console.log("app request user: ",req.user)
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.use('/admin', adminRoutes);
@@ -32,5 +40,6 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoConnect(() => {
+  // if()
   app.listen(3000);
 });
